8f8c346204fb4e589b95cf9582935006445f822e
MKaaNa <mkaanaltin@gmail.com>
2024-08-06 16:33:54 +0300

    GET 400 hatası giderildi.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { BASE_API_URL } from '../utils/constants';
 
+
 // Axios instance
 const api = axios.create({
   baseURL: BASE_API_URL, // Temel URL
   headers: {
     'Content-Type': 'application/json',
-
+    // Diğer gerekli başlıklar buraya eklenebilir
   }
 });
 
